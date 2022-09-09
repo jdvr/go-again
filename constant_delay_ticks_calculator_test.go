cdae3098e44a4d2fafd0b6cfb82f2c5dbe1a42e0
Juan Vega <juandavidvegarguez@gmail.com>
2022-09-09 17:32:40 +0200

    improved contant_delay_ticks_calculator.go

---
+++ constant_delay_ticks_calculator_test.go
@@ -0,0 +1,40 @@
+package again
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestConstantDelayTicksCalculator_Next(t *testing.T) {
+	t.Run("delay is always the same", func(t *testing.T) {
+		ticksCalculator := MustConstantDelayTicksCalculator(500*time.Millisecond, 1*time.Hour)
+		expected := []Tick{
+			{Next: 500 * time.Millisecond},
+			{Next: 500 * time.Millisecond},
+			{Next: 500 * time.Millisecond},
+			{Next: 500 * time.Millisecond},
+		}
+
+		var generated []Tick
+		for i := 0; i < len(expected); i++ {
+			generated = append(generated, ticksCalculator.Next())
+		}
+
+		require.Equal(t, expected, generated)
+	})
+	t.Run("stop when timed out", func(t *testing.T) {
+		ticksCalculator := MustConstantDelayTicksCalculator(500*time.Millisecond, 1*time.Nanosecond)
+
+		require.Equal(t, Tick{Stop: true}, ticksCalculator.Next())
+	})
+	t.Run("panics for 0 config", func(t *testing.T) {
+		require.Panics(t, func() {
+			MustConstantDelayTicksCalculator(0, time.Second)
+		})
+		require.Panics(t, func() {
+			MustConstantDelayTicksCalculator(time.Hour, 0)
+		})
+	})
+}
--- contant_delay_ticks_calculator.go
+++ contant_delay_ticks_calculator.go
@@ -5,20 +5,39 @@ import (
 )
 
 type constantDelayTicksCalculator struct {
-	delay time.Duration
+	delay   time.Duration
+	timeout time.Duration
+
+	startAt time.Time
+	clock   clock
 }
 
-func newConstantDelayTicksCalculator(delay time.Duration) TicksCalculator {
-	return constantDelayTicksCalculator{
-		delay: 0,
+func MustConstantDelayTicksCalculator(delay time.Duration, timeout time.Duration) TicksCalculator {
+	if delay == 0 || timeout == 0 {
+		panic("delay and timeout must be set")
+	}
+
+	clock := systemClock{}
+	return &constantDelayTicksCalculator{
+		delay:   delay,
+		timeout: timeout,
+		startAt: clock.Now(),
+		clock:   clock,
 	}
 }
 
-func (c constantDelayTicksCalculator) Next() Tick {
+func (c *constantDelayTicksCalculator) Next() Tick {
+	elapsed := c.clock.Now().Sub(c.startAt)
+	if elapsed > c.timeout {
+		return Tick{Stop: true}
+	}
+
 	return Tick{
 		Next: c.delay,
 		Stop: false,
 	}
 }
 
-func (c constantDelayTicksCalculator) Reset() {}
+func (c *constantDelayTicksCalculator) Reset() {
+	c.startAt = c.clock.Now()
+}
